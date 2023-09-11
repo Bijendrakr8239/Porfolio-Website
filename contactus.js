8aca9a59f1f8e61ec67a66fa1086f108daf85b7f
Bijendra Kumar <bijendrakr8239@gmail.com>
2023-09-11 22:05:39 +0530

    Project Updated

--- contactus.js
+++
@@ -1,3 +0,0 @@
-var name1=prompt("Enter First person's name");
-var name2=prompt("Enter Second person's name");
-alert("Love Percentage Between "+name1+" and "+name2+" is "+(Math.floor(Math.random()*100)+1)+"%");
